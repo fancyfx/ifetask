ab89b1c7f1712a6b6692770d21b01d07c8ed23f9
方希 <412984230@qq.com>
2016-07-27 23:36:32 +0800

    完成飞船创建方法

--- task26/js/main.js
+++ task26/js/main.js
@@ -1,3 +1,26 @@
+
+  var div = document.getElementById('test');
+  var mybutton = div.getElementsByTagName('button');
+
+var airship1 = new SetAirship();
+
+mybutton[0].onclick = function () {
+    airship1.fly();
+}
+mybutton[1].onclick = function () {
+    airship1.stateType = "stop";
+}
+mybutton[2].onclick = function () {
+    airship1.onAndOff = "off";
+    airship1.stateType = "stop";
+}
+mybutton[3].onclick = function () {
+    alert("飞行状态："+airship1.stateType +"\n" +
+          "能量："+ airship1.energy +"\n" +
+          "能源状态："+airship1.onAndOff);
+}
+
+
 /**
  *  创建飞船的 的构造函数
  *
@@ -6,44 +29,59 @@
  * 	飞船的方法：飞行(fly)，停止(flyStop)， 自毁系统(destroySys)
  */
 
-function SetAirship(id,time) {
-    this.flyspeed = "20px";
-    this.energy = 98;
-    this.id = id;
-    this.stateType =  "stop";
+function SetAirship(id) {
+    this.flyspeed = "20px"; //飞行速度
+    this.energy = 100;  //初始能量
+    this.id = id;  //飞船id
+    this.stateType =  "stop"; //初始 飞行状态
+    this.onAndOff = "off"; // 太阳能系统 控制开关
 }
+// 飞船 飞行方法
 SetAirship.prototype.fly = function () {
     if (this.stateType === "fly") return;
-    this.stateType = "fly";
-    // 能量 每秒 增加 2
-    increaseEnergy(this.energy);
+    if (this.energy > 0 ) {
+      // 设置飞行状态
+      this.stateType = "fly";
+      // 打开 太阳能充电系统
+      if (this.onAndOff === "off") {
+         this.onAndOff = "on";
+         this.sunEnergySys();
+      }
+      // 飞船能量消耗系统
+      this.energySys();
+
+    }else {
+      console.log("飞船需要充能无法起飞 当前能量：" + this.energy);
+    }
+
 }
+// 飞船停止方法
 SetAirship.prototype.flyStop = function () {
     if (this.stateType === "stop") return;
     this.stateType = "stop";
-    if(this.energy !== 100) increaseEnergy(this.energy);
 }
+// 飞船能量消耗系统 每秒 -5
+SetAirship.prototype.energySys = function () {
+    if (this.energy >= 5 ) {
+      this.energy -= 5;
+      console.log("当前能量正在减少，剩余：" + this.energy);
+    }else {
+      this.stateType = "stop";
+    }
 
-// 飞船 能量增长方法 ， 每秒 增加 2 ;
-function increaseEnergy(numEnergy) {
-    function incrementNumber() {
-      numEnergy += 2;
-      if (numEnergy < 100) {
-        setTimeout(incrementNumber, 1000);
-      }
+    if (this.energy > 0 && this.stateType ===  "fly") {
+      setTimeout(this.energySys.bind(this), 1000);  // setTimeout的执行函数里面的this 指向windows 这里用bind() 解决这个问题
+    }else {
+      this.stateType = "stop";
     }
-    setTimeout(incrementNumber, 1000);
 }
-// 飞船 飞行时 能量减少方法 ， 每秒 减少 5 ;
-function reduceEnergy(numEnergy) {
-    function incrementNumber() {
-      numEnergy -= 5;
-      if (numEnergy > 0) {
-        setTimeout(incrementNumber, 1000);
-      }
+//  飞船太阳能充电系统 每秒 +2
+SetAirship.prototype.sunEnergySys = function () {
+    if (this.energy !== 100) {
+      this.energy += 2;
+      console.log("当前能量正在增加，剩余：" + this.energy);
     }
-    setTimeout(incrementNumber, 1000);
+    if (this.onAndOff === "on") {
+      setTimeout(this.sunEnergySys.bind(this), 1000);  // setTimeout的执行函数里面的this 指向windows 这里用bind() 解决这个问题
+    }
 }
-var airship1 = new SetAirship();
-airship1.flyspeed = "50px";
-alert(airship1.stateType);
