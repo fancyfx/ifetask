08cce6b2912e407da7461b3f0a211f3cd52868b0
方希 <412984230@qq.com>
2016-08-10 22:00:33 +0800

    新增 棋子对象 pieces

--- task34/js/main.js
+++ task34/js/main.js
@@ -2,10 +2,10 @@ window.onload = function () {
  addGrid();
  addNodePieces();
 };
-var startVal = '7,7', //位置
-    borderValue = 'piecesTop',  //方块方向  piecesTop:上  piecesRight:右 piecesBottom:下 piecesLeft:左
-    moveVal = 'move-up',  //动画移动方向
-    onBloo = true; //命令执行状态 防止重复点击
+// var startVal = '7,7', //位置
+//     borderValue = 'piecesTop',  //方块方向  piecesTop:上  piecesRight:右 piecesBottom:下 piecesLeft:左
+//     moveVal = 'move-up',  //动画移动方向
+//     onBloo = true; //命令执行状态 防止重复点击
 /**
  * 获取页面元素 控制棋子
  */
@@ -25,76 +25,13 @@ var startVal = '7,7', //位置
           controlPieces();
           break;
         case "TUN LEF":
-          switch (borderValue) {
-            case 'piecesTop':
-              borderValue = 'piecesLeft';
-              break;
-            case 'piecesBottom':
-              borderValue = 'piecesRight';
-              break;
-            case 'piecesLeft':
-              borderValue = 'piecesBottom';
-              break;
-            case 'piecesRight':
-              borderValue = 'piecesTop';
-              break;
-          }
-          if (onBloo) {
-            rotateAnimation('rotate-left');
-            setTimeout(function () {
-              removeNode(startVal);
-              addNodePieces();
-              onBloo = true;
-            }, 1000);
-          }
+          pieces.rotateLeft();
           break;
         case "TUN RIG":
-        switch (borderValue) {
-          case 'piecesTop':
-            borderValue = 'piecesRight';
-            break;
-          case 'piecesBottom':
-            borderValue = 'piecesLeft';
-            break;
-          case 'piecesLeft':
-            borderValue = 'piecesTop';
-            break;
-          case 'piecesRight':
-            borderValue = 'piecesBottom';
-            break;
-        }
-        if (onBloo) {
-          rotateAnimation('rotate-right');
-          setTimeout(function () {
-            removeNode(startVal);
-            addNodePieces();
-            onBloo = true;
-          }, 1000);
-        }
+          pieces.rotateRight();
           break;
         case "TUN BAC":
-            switch (borderValue) {
-              case 'piecesTop':
-                borderValue = 'piecesBottom';
-                break;
-              case 'piecesBottom':
-                borderValue = 'piecesTop';
-                break;
-              case 'piecesLeft':
-                borderValue = 'piecesRight';
-                break;
-              case 'piecesRight':
-                borderValue = 'piecesLeft';
-                break;
-            }
-            if (onBloo) {
-              rotateAnimation('rotate-back');
-              setTimeout(function () {
-                removeNode(startVal);
-                addNodePieces();
-                onBloo = true;
-              }, 1000);
-            }
+          pieces.rotateBack();
           break;
       }
     };
@@ -103,12 +40,12 @@ var startVal = '7,7', //位置
  * 添加棋子页面元素
  */
 function addNodePieces() {
-  var startNod = document.getElementById(startVal);
+  var startNod = document.getElementById(pieces.startVal);
   var backNod = startNod.getElementsByTagName('span');
   if (backNod.length < 1) {
     var spanNod1 = document.createElement('span');
     startNod.appendChild(spanNod1);
-    spanNod1.setAttribute('class', 'pieces '+ borderValue);
+    spanNod1.setAttribute('class', 'pieces '+ pieces.borderValue);
   }
 }
 /**
@@ -136,83 +73,22 @@ function addGrid() {
 }
 
 /**
- * 保存棋子位置和方向方法
- * 初始化添加位置
- */
-//
-// function addPieces() {
-//
-//     // 初始化棋子位置
-//     addNodePieces(startVal);
-//
-// }
-
-/**
- * 棋子前进 处理逻辑
+ * GO 命令 处理逻辑
  */
 function controlPieces() {
     var val,sp;
-    switch (borderValue) {
+    switch (pieces.borderValue) {
       case 'piecesTop':
-        sp = startVal.split(",");
-        val = parseFloat(sp[0]) - 1;
-        if (val > 0 && onBloo) {
-          moveVal = 'move-up';
-           moveAnimation();
-          setTimeout(function () {
-            removeNode(startVal);
-            startVal = val + "," + sp[1];
-            // 更新棋子
-            addNodePieces();
-            onBloo = true;
-          },1000);
-        }
+        pieces.moveTop();
         break;
       case 'piecesBottom':
-        sp = startVal.split(",");
-        val = parseFloat(sp[0]) + 1;
-        if (val < 11 && onBloo) {
-          moveVal = 'move-down';
-          moveAnimation();
-          setTimeout(function () {
-            removeNode(startVal);
-            startVal = val + "," + sp[1];
-            // 更新棋子
-            addNodePieces();
-            onBloo = true;
-          },1000);
-        }
+        pieces.moveBottom();
         break;
       case 'piecesLeft':
-        sp = startVal.split(",");
-        val = parseFloat(sp[1]) - 1;
-        if ( val > 0 && onBloo) {
-          moveVal = 'move-left';
-          moveAnimation();
-          setTimeout(function () {
-            removeNode(startVal);
-            startVal = sp[0] + "," + val;
-            // 更新棋子
-            addNodePieces();
-            onBloo = true;
-          },1000);
-
-        }
+        pieces.moveLeft();
         break;
       case 'piecesRight':
-          sp = startVal.split(",");
-          val = parseFloat(sp[1]) + 1;
-        if ( val < 11 && onBloo) {
-          moveVal = 'move-right';
-          moveAnimation();
-          setTimeout(function () {
-            removeNode(startVal);
-            startVal = sp[0] + "," + val;
-            // 更新棋子
-            addNodePieces();
-            onBloo = true;
-          },1000);
-        }
+        pieces.moveRight();
         break;
     }
 }
@@ -220,8 +96,8 @@ function controlPieces() {
 /**
  * 删除原来位置上的棋子
  */
-function removeNode(startVal) {
-   var parNode = document.getElementById(startVal);
+function removeNode(val) {
+   var parNode = document.getElementById(val);
    var oldNode = parNode.getElementsByTagName('span');
     parNode.removeChild(oldNode[0]);
 }
@@ -229,19 +105,19 @@ function removeNode(startVal) {
 /**
  * 运行棋子移动动画
  */
-function moveAnimation() {
-    var parNode = document.getElementById(startVal);
+function moveAnimation(val) {
+    var parNode = document.getElementById(pieces.startVal);
     var spanNode = parNode.getElementsByTagName('span');
-    spanNode[0].style.animation = moveVal+' 1s 1 linear forwards';
-    onBloo = false;
+    spanNode[0].style.animation = val+' 1s 1 linear forwards';
+    pieces.onBloo = false;
 }
 
 /**
  * 运行旋转动画
  */
- function rotateAnimation(rotateVal) {
-     var parNode = document.getElementById(startVal);
+ function rotateAnimation(val) {
+     var parNode = document.getElementById(pieces.startVal);
      var spanNode = parNode.getElementsByTagName('span');
-     spanNode[0].style.animation = rotateVal+' 1s 1 linear forwards';
-     onBloo = false;
+     spanNode[0].style.animation = val+' 1s 1 linear forwards';
+     pieces.onBloo = false;
  }
--- task34/js/pieces.js
+++ task34/js/pieces.js
@@ -1,37 +1,148 @@
 /**
- * 棋子对象 定义棋子的熟悉和方法
+ * 棋子对象 定义棋子的属性和方法
  */
 var pieces = {
       startVal :'7,7',            //位置
       borderValue : 'piecesTop',  //方块方向  piecesTop:上  piecesRight:右 piecesBottom:下 piecesLeft:左
-      moveVal : 'move-up',        //动画移动方向
       onBloo : true   ,            //命令执行状态 防止重复点击
       // 向上移动的方法
       moveTop : function () {
-         
+        var val,sp;
+        sp = pieces.startVal.split(",");
+        val = parseFloat(sp[0]) - 1;
+        if (val > 0 && pieces.onBloo) {
+           moveAnimation('move-up');
+          setTimeout(function () {
+            removeNode(pieces.startVal);
+            pieces.startVal = val + "," + sp[1];
+            // 更新棋子
+            addNodePieces();
+            pieces.onBloo = true;
+          },1000);
+        }
       },
       // 向下移动的方法
       moveBottom : function () {
-
+        var val,sp;
+        sp = pieces.startVal.split(",");
+        val = parseFloat(sp[0]) + 1;
+        if (val < 11 && pieces.onBloo) {
+          moveAnimation('move-down');
+          setTimeout(function () {
+            removeNode(pieces.startVal);
+            pieces.startVal = val + "," + sp[1];
+            // 更新棋子
+            addNodePieces();
+            pieces.onBloo = true;
+          },1000);
+        }
       },
       // 向左移动的方法
       moveLeft : function () {
+        var val,sp;
+        sp = pieces.startVal.split(",");
+        val = parseFloat(sp[1]) - 1;
+        if ( val > 0 && pieces.onBloo) {
+          moveAnimation('move-left');
+          setTimeout(function () {
+            removeNode(pieces.startVal);
+            pieces.startVal = sp[0] + "," + val;
+            // 更新棋子
+            addNodePieces();
+            pieces.onBloo = true;
+          },1000);
 
+        }
       },
       // 向右移动的方法
       moveRight : function () {
-
+        var val,sp;
+        sp = pieces.startVal.split(",");
+        val = parseFloat(sp[1]) + 1;
+      if ( val < 11 && pieces.onBloo) {
+        moveAnimation('move-right');
+        setTimeout(function () {
+          removeNode(pieces.startVal);
+          pieces.startVal = sp[0] + "," + val;
+          // 更新棋子
+          addNodePieces();
+          pieces.onBloo = true;
+        },1000);
+      }
       },
       // 向左旋转
       rotateLeft : function () {
-
+        switch (pieces.borderValue) {
+          case 'piecesTop':
+            pieces.borderValue = 'piecesLeft';
+            break;
+          case 'piecesBottom':
+            pieces.borderValue = 'piecesRight';
+            break;
+          case 'piecesLeft':
+            pieces.borderValue = 'piecesBottom';
+            break;
+          case 'piecesRight':
+            pieces.borderValue = 'piecesTop';
+            break;
+        }
+        if (pieces.onBloo) {
+          rotateAnimation('rotate-left');
+          setTimeout(function () {
+            removeNode(pieces.startVal);
+            addNodePieces();
+            pieces.onBloo = true;
+          }, 1000);
+        }
       },
       // 向右旋转
       rotateRight : function () {
-
+        switch (pieces.borderValue) {
+          case 'piecesTop':
+            pieces.borderValue = 'piecesRight';
+            break;
+          case 'piecesBottom':
+            pieces.borderValue = 'piecesLeft';
+            break;
+          case 'piecesLeft':
+            pieces.borderValue = 'piecesTop';
+            break;
+          case 'piecesRight':
+            pieces.borderValue = 'piecesBottom';
+            break;
+        }
+        if (pieces.onBloo) {
+          rotateAnimation('rotate-right');
+          setTimeout(function () {
+            removeNode(pieces.startVal);
+            addNodePieces();
+            pieces.onBloo = true;
+          }, 1000);
+        }
       },
       // 旋转180
       rotateBack : function () {
-
+        switch (pieces.borderValue) {
+          case 'piecesTop':
+            pieces.borderValue = 'piecesBottom';
+            break;
+          case 'piecesBottom':
+            pieces.borderValue = 'piecesTop';
+            break;
+          case 'piecesLeft':
+            pieces.borderValue = 'piecesRight';
+            break;
+          case 'piecesRight':
+            pieces.borderValue = 'piecesLeft';
+            break;
+        }
+        if (pieces.onBloo) {
+          rotateAnimation('rotate-back');
+          setTimeout(function () {
+            removeNode(pieces.startVal);
+            addNodePieces();
+            pieces.onBloo = true;
+          }, 1000);
+        }
       }
 };
