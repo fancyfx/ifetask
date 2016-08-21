92f85d88ed93290e17c9765c52624d04e73fdb10
方希 <412984230@qq.com>
2016-08-22 01:18:54 +0800

    添加注释

--- task40/js/datePicker.js
+++ task40/js/datePicker.js
@@ -1,9 +1,30 @@
+/**
+ * [日历组件]
+ * 调用示例(配置说明)：
+ * // 创建 日历
+ * var date = new DatePicker({
+ *        id:"date",                   // 日历组件 dom class（必须否则无法 创建 由于用的是jQuery选择器 id请+ # class + .）
+ *        maxDate:'2016/8/31',         // 可选 时间段最大时间（非必选，注意格式一定要相同）
+ *         minDate:'2016/8/1'          // 可选 时间段最小时间（非必选，注意格式一定要相同）
+ *    });
+ *  // 设置日历时间 （注意：设置参数一定要和下面一样）
+ *  date.setAndGetDate().setDate(
+ *      {
+ *        year:2016,
+ *        month:8,
+ *        day:1
+ *      }
+ *    );
+ *  // 获取日历时间
+ *  date.setAndGetDate().getDate();
+ */
+
+
 (function(window,document){
    function DatePicker(options){
-      //传入配置的中的参数
       // 创建日历组件的 页面元素 id
       if (!options.id) return;
-      this.nodeid = '.' + options.id ;
+      this.nodeid = options.id ;
       this.td = [];               // 存储 dom 的td 对象
       // 初始化时日历当前时间
       this.year = new Date().getFullYear();    // 当前时间 年
@@ -46,18 +67,18 @@
         },
         createDom:function(){
            // 创建dom对象或者创建html片段或者创建template
-
           //  添加 年和月
-           $(this.nodeid).append("<div class='head'>"+
+          $(this.nodeid).append("<div class='date'></div>");
+           $(this.nodeid + ' .date').append("<div class='head'>"+
             "<div class='year'><i class='year-left'><</i><span class='year-data'>年</span><i class='year-right'>></i></div> "+
              "<div class='month'><i class='month-left'><</i><span class='month-data'>月</span><i class='month-right'>></i></div></div>");
-           $(this.nodeid).append("<table></table>");
+           $(this.nodeid+ ' .date').append("<table></table>");
           //  添加星期
-           $(this.nodeid + ' table').append("<tr><th>日</th><th>一</th><th>二</th><th>三</th><th>四</th><th>五</th><th>六</th></tr>");
+           $(this.nodeid + ' .date table').append("<tr><th>日</th><th>一</th><th>二</th><th>三</th><th>四</th><th>五</th><th>六</th></tr>");
           // 添加 天的包裹元素 td
            for (var i = 0; i < 6; i++) {
              var tr = document.createElement('tr');
-             $(this.nodeid + ' table').append($(tr));
+             $(this.nodeid + ' .date table').append($(tr));
             for (var j = 0; j < 7; j++) {
               var td = document.createElement('td');
               tr.appendChild(td);
@@ -69,19 +90,19 @@
            //事件绑定
          var that = this;
         //  年减少
-         $(this.nodeid + ' .head .year-left').click(function () {
+         $(this.nodeid + ' .date .head .year-left').click(function () {
              if (that.year > 0 ) {
                  that.year--;
                  that.render();
              }
          });
         //  年增加
-        $(this.nodeid + ' .head .year-right').click(function () {
+        $(this.nodeid + ' .date .head .year-right').click(function () {
             that.year++;
             that.render();
       });
         //  月减少
-         $(this.nodeid + ' .head .month-left').click(function () {
+         $(this.nodeid + ' .date .head .month-left').click(function () {
              if (that.month > 1) {
                that.month--;
              }else {
@@ -91,7 +112,7 @@
               that.render();
          });
         //  月增加
-        $(this.nodeid+ ' .head .month-right').click(function () {
+        $(this.nodeid+ ' .date .head .month-right').click(function () {
             if (that.month < 12) {
               that.month++;
             }else {
@@ -101,7 +122,7 @@
              that.render();
         });
         //  点击 td 选择 日期
-        $(this.nodeid + ' table td').click(function () {
+        $(this.nodeid + ' .date table td').click(function () {
               if (!this.firstChild) return;
               if (this.firstChild.getAttribute('class') === 'clickable') {
                 that.selectDate.year = that.year;
@@ -118,9 +139,9 @@
           //渲染函数，更新数据或样式
           var nowDate = calculate(this.year,this.month,this.day);
           // 添加 年
-          $(this.nodeid + ' .head .year-data').get(0).innerHTML= this.year+'年';
+          $(this.nodeid + ' .date .head .year-data').get(0).innerHTML= this.year+'年';
           // 添加 月
-          $(this.nodeid + ' .head .month-data').get(0).innerHTML= this.month+'月';
+          $(this.nodeid + ' .date .head .month-data').get(0).innerHTML= this.month+'月';
 
           // 重置 td
           for (var j = 0; j < this.td.length; j++) {
